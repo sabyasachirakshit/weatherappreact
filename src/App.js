04f56cecabb1217544c048e9e35b4ff04cf8d546
sabyasachirakshit <sabyasachi@121connection.com>
2023-03-21 11:19:19 +0530

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=India&aqi=no`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-screen">
+      <h2>SR Weather App</h2>
+      {weatherData && (
+        <div className="result-screen">
+          <h2>Location: {weatherData.location.name}</h2>
+          <h2>Temperature: {weatherData.current.temp_c}°C</h2>
+          <h2>Humidity: {weatherData.current.humidity}%</h2>
+        </div>
+      )}
     </div>
   );
 }
